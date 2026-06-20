989b2ac8706722be58c56ca9257bc01b157f86e2
Paul Ciocan <paul.ciocan@3pillarglobal.com>
2026-06-20 11:38:30 +0300

    Changed import service to be more abstract

--- app/jobs/coffee_shop_import_job.rb
+++ app/jobs/coffee_shop_import_job.rb
@@ -4,15 +4,21 @@ class CoffeeShopImportJob < ApplicationJob
   queue_as :default
 
   def perform(url)
-    ImporterService.new(importer: build_importer, model: CoffeeShop).call(url)
+    ImportService.new(
+      importer: build_importer,
+      model: CoffeeShop,
+      unique_by: [:x, :y],
+      update_only: [:name]
+    ).call(url)
   end
 
   private
 
   def build_importer
-    Importer::CsvImporter.new(
-      downloader: Importer::Downloaders::HttpDownloader.new,
-      parser: Importer::Parsers::CsvParser.new
+    Import::CsvImporter.new(
+      downloader: Import::Downloaders::HttpDownloader.new,
+      parser: Import::Parsers::CsvParser.new,
+      mapper: Import::Mappers::CoffeeShopCsvMapper.new
     )
   end
 end
